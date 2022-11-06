b675266f1cbfef58684f19c2e7b1e90dfca02230
Geoffroy Warin <code@geowarin.com>
2022-11-06 17:28:27 +0100

    use correct test runner from https://github.com/van800/godot-demo-projects/tree/net6/mono/dodge_the_creeps/RiderTestRunner

--- RiderTestRunner/Runner.cs
+++ RiderTestRunner/Runner.cs
@@ -1,5 +1,8 @@
 using System;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
 using Godot;
 using Environment = System.Environment;
 using Thread = System.Threading.Thread;
@@ -8,10 +11,18 @@ using Thread = System.Threading.Thread;
 namespace RiderTestRunner
 {
     // ReSharper disable once UnusedType.Global
-    public partial class Runner : Node // for GodotXUnit use: public class Runner : GodotTestRunner. https://github.com/fledware/GodotXUnit/issues/8#issuecomment-929849478
+    public partial class NetCoreRunner : Node // for GodotXUnit use: public partial class Runner : GodotTestRunner. https://github.com/fledware/GodotXUnit/issues/8#issuecomment-929849478
     {
+        private string _runnerAssemblyPath;
         public override void _Ready()
         {
+
+            //while (!Debugger.IsAttached)
+            {
+                
+            }
+
+            
             // GDU.Instance = this; // for GodotXUnit https://github.com/fledware/GodotXUnit/issues/8#issuecomment-929849478
             var textNode = GetNode<RichTextLabel>("RichTextLabel");
             foreach (var arg in OS.GetCmdlineArgs())
@@ -21,14 +32,13 @@ namespace RiderTestRunner
 
             if (OS.GetCmdlineArgs().Length < 4)
                 return;
-
-            var unitTestAssembly = OS.GetCmdlineArgs()[2];
+            
             var unitTestArgs = OS.GetCmdlineArgs()[4].Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries).ToArray();
-            // https://docs.microsoft.com/en-us/dotnet/api/system.appdomain.executeassembly?view=netframework-4.7.2
-            AppDomain currentDomain = AppDomain.CurrentDomain;
+            _runnerAssemblyPath = OS.GetCmdlineArgs()[2];
+            AssemblyLoadContext.Default.Resolving += DefaultOnResolving;
             var thread = new Thread(() =>
             {
-                currentDomain.ExecuteAssembly(unitTestAssembly, unitTestArgs);
+                AppDomain.CurrentDomain.ExecuteAssembly(_runnerAssemblyPath, unitTestArgs);
                 GetTree().Quit();
             });
             thread.Start();
@@ -36,11 +46,21 @@ namespace RiderTestRunner
             WaitForThreadExit(thread);
         }
 
+        private Assembly DefaultOnResolving(AssemblyLoadContext context, AssemblyName assemblyName)
+        { 
+            var dir = new FileInfo(_runnerAssemblyPath).Directory;
+            if (dir == null) return null;
+            var file = new FileInfo(Path.Combine(dir.FullName, $"{assemblyName.Name}.dll"));
+            if (file.Exists) 
+                return AssemblyLoadContext.Default.LoadFromAssemblyPath(file.FullName);
+            return null;
+        }
+
         private async void WaitForThreadExit(Thread thread)
         {
             while (thread.IsAlive)
             {
-                await ToSignal(GetTree().CreateTimer(1), "timeout");
+                await ToSignal(GetTree().CreateTimer(100), "timeout");
             }
         }
     }
