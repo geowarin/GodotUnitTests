77558eae952b65bf363871eca0b2b0903a21d4d7
Geoffroy Warin <code@geowarin.com>
2023-05-23 12:52:57 +0200

    use project/solution_directory="../.." to get a project layout

--- projects/Game/Main.cs
+++ projects/Game/Main.cs
@@ -6,6 +6,7 @@ public partial class Main : Node3D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		GD.Print("Hello!!");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
